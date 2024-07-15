835d8a6219d67d6e62bc9e1b398503da347f31b2
Artur Castro <artur-castro@live.com>
2024-07-14 22:36:57 -0300

    aula 02

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,18 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'hi world!'}
+
+
+@app.get('/hi', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hi():
+    return '''<html><h3>Hi world!</h3></html>'''
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,12 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # assert (afirmação)
     assert response.json() == {'message': 'hi world!'}
+
+
+def test_hi_deve_retornar_ok_e_hi_world():
+    client = TestClient(app)
+
+    response = client.get('/hi')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '<html><h3>Hi world!</h3></html>'
