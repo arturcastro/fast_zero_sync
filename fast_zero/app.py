a402cd4af731720c3c51891cd1ee31ec8e43fc02
Tui Castro <artur-castro@live.com>
2025-02-07 23:59:11 -0300

    Aula 2 - Criando rota com retorno HTML e teste

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,18 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'hi'}
+    return {'message': 'hi', 'batata': 'frita'}
+
+
+@app.get('/html', response_class=HTMLResponse)
+def html_hi():
+    return '<h3>Hi!</h3>'
--- tests/test_app.py
+++ tests/test_app.py
@@ -13,3 +13,13 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
     # assert (afirmação)
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'hi'}
+
+
+def test_html_hi_deve_retornar_ok_e_ola_mundo_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '<h3>Hi!</h3>'
+    assert response.headers['content-type'] == 'text/html; charset=utf-8'
